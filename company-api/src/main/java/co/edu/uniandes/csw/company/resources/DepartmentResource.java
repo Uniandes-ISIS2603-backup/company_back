f5b402b7af0e15dc96a2a72cf52af0d8db6d288b
rcasalla <rcasalla@hotmail.com>
2016-10-26 21:58:43 -0500

    fixed url departmentemployee resource

--- company-api/src/main/java/co/edu/uniandes/csw/company/resources/DepartmentResource.java
+++ company-api/src/main/java/co/edu/uniandes/csw/company/resources/DepartmentResource.java
@@ -103,6 +103,7 @@ public class DepartmentResource {
      *
      */
     @GET
+     @Path("departments")
     public List<DepartmentDetailDTO> getDepartments() {
         existsCompany(companyId);
 
